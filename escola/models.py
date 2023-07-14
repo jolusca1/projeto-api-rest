d4f4f1aa84aa5c2006a94aa1a8581511eeeeef81
João Lucas <110925879+jolusca1@users.noreply.github.com>
2023-07-13 22:04:37 -0300

    serializer

--- escola/models.py
+++ escola/models.py
@@ -1,2 +1,35 @@
 from django.db import models
 
+class Aluno(models.Model):
+    nome = models.CharField(
+        max_length=30, null=False, blank=False
+        )
+    rg = models.CharField(
+        max_length=9
+        )
+    cpf = models.CharField(
+        max_length=11
+        )
+    data_nascimento = models.DateField()
+    
+    def __str__(self):
+        return self.nome
+    
+class Curso(models.Model):
+    
+    NIVEL = [
+        ('B', 'Básico'),
+        ('I', 'Intermediário'),
+        ('A', 'Avançado')
+    ]
+    
+    codigo_curso = models.CharField(
+        max_length=10, null=False, blank=False
+        )
+    descricao_curso = models.CharField(
+        max_length=100, null=False, blank=False
+        )
+    nivel_curso = models.CharField(max_length=1, choices=NIVEL, blank=False, null=False, default='B')
+    
+    def __str__(self):
+        return self.descricao_curso
---
+++ escola/serializer.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from escola.models import Aluno, Curso
+
+
+class AlunoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Aluno
+        fields = [
+            'id', 'nome', 'rg', 'cpf'
+            ]
+        
+class CursoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Curso
+        fields = '__all__'
