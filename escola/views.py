b43cb412c7c5cf2a59b2c5757fb2ea07d58c68d3
João Lucas <110925879+jolusca1@users.noreply.github.com>
2023-07-13 22:24:29 -0300

    serializers

--- escola/views.py
+++ escola/views.py
@@ -1,9 +1,15 @@
-from django.http import JsonResponse
+from rest_framework import viewsets
+from escola.models import Aluno, Curso
+from escola.serializer import AlunoSerializer, CursoSerializer
 
-def alunos(request):
-    if request.method == 'GET':
-        aluno = {
-            'id': 1,
-            'nome': 'Joao'
-        }
-        return JsonResponse(aluno)
+
+class AlunosViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os alunos e alunas"""
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
+    
+    
+class CursosViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os cursos"""
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
